4b55b971492e83051b4468a88dc339849c253297
Rosepham9x <rose.cuxi@gmail.com>
2018-10-26 20:50:38 +0700

    update file

---
+++ Exp1.js
@@ -0,0 +1,7 @@
+var patt =/hello/;
+if(patt.test ("Chào mừng bạn đến với JavaScript")) {
+    document.write("Trong chuỗi có chữ hello");
+}
+else {
+    document.write('Trong chuỗi không có chữ hello');
+}
---
+++ Exp2.js
@@ -0,0 +1,6 @@
+if (/n+/.test("hello n")) {
+    document.write('Trong chuỗi có chữ n');
+}
+else {
+    document.write ('Trong chuỗi không có chữ n');
+}
